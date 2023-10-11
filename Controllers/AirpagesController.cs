c2bd7e8c715337a935a9c8f522c22f16b97a74e0
Ricardo.Vaca <ricardo.vaca@udla.edu.ec>
2023-10-11 00:42:31 -0500

    Idea Proyecto Final

---
+++ Controllers/AirpagesController.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace finalAirpro.Controllers
+{
+    public class AirpagesController : Controller
+    {
+        // GET: AirpagesController1
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: AirpagesController1/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: AirpagesController1/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: AirpagesController1/Create
+        
+
+        // GET: AirpagesController1/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: AirpagesController1/Edit/5
+        
+
+        // GET: AirpagesController1/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+       
+    }
+}
---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace finalAirpro.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
